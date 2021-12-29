8f3497437726ab56a47eb7e4d7637797fb0f764e
yumtaufikhidayat <yumtaufik1997@gmail.com>
2021-12-29 19:41:41 +0700

    Adding Splash Screen.

---
+++ app/src/main/java/com/taufik/asean/ui/activity/SplashscreenActivity.kt
@@ -0,0 +1,47 @@
+package com.taufik.asean.ui.activity
+
+import android.content.Intent
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.taufik.asean.databinding.ActivitySplashscreenBinding
+
+
+class SplashscreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashscreenBinding
+    private lateinit var handler: Handler
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashscreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSplashscreen()
+        setAppVersion()
+    }
+
+    private fun setSplashscreen() {
+        handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1000)
+    }
+
+    private fun setAppVersion() {
+        binding.apply {
+            try {
+                val pInfo: PackageInfo = packageManager.getPackageInfo(packageName, 0)
+                val appVersion = pInfo.versionName
+                tvAppVersion.text = appVersion
+            } catch (e: PackageManager.NameNotFoundException) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
