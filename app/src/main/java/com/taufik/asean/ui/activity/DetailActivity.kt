ed0c3a7a64e6c8439a40e6f41a72815b0aae9110
yumtaufikhidayat <yumtaufik1997@gmail.com>
2021-12-30 10:33:19 +0700

    Updating detail.

--- app/src/main/java/com/taufik/asean/ui/activity/DetailActivity.kt
+++ app/src/main/java/com/taufik/asean/ui/activity/DetailActivity.kt
@@ -1,13 +1,18 @@
 package com.taufik.asean.ui.activity
 
+import android.content.Intent
 import android.graphics.Typeface
+import android.net.Uri
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DecodeFormat
 import com.bumptech.glide.request.RequestOptions
 import com.bumptech.glide.request.target.Target
+import com.taufik.asean.R
 import com.taufik.asean.data.Country
 import com.taufik.asean.databinding.ActivityDetailBinding
 
@@ -69,9 +74,29 @@ class DetailActivity : AppCompatActivity() {
         }
     }
 
+    private fun openInBrowser() {
+        try {
+            val link = "http://setnas-asean.id/profil-negara-anggota-asean"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(link))
+            startActivity(Intent.createChooser(intent, "Open with:"))
+        } catch (e: Exception) {
+            Toast.makeText(
+                this,
+                "Silakan install browser terlebih dahulu",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_detail, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             android.R.id.home -> onBackPressed()
+            R.id.action_open_in_browser -> openInBrowser()
         }
         return super.onOptionsItemSelected(item)
     }
