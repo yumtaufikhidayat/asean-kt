afb79391349f98a8867d6422a768e2f0eedb7fed
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-08-06 19:59:14 +0700

    Add view model variable and show profile data.

--- app/src/main/java/com/yumtaufikhidayat/asean/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/yumtaufikhidayat/asean/ui/profile/ProfileFragment.kt
@@ -1,19 +1,26 @@
 package com.yumtaufikhidayat.asean.ui.profile
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.yumtaufikhidayat.asean.R
 import com.yumtaufikhidayat.asean.databinding.FragmentProfileBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class ProfileFragment : Fragment() {
 
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel by viewModels<ProfileViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,9 +32,38 @@ class ProfileFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        handleToolbar()
+        setProfile()
+    }
+
+    private fun handleToolbar() {
+        binding.toolbarProfile.imgBack.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+
+    private fun setProfile() {
+        binding.apply {
+            lifecycleScope.launch {
+                viewModel.getProfile().collect { profile ->
+                    imgProfile.setImageDrawable(
+                        ContextCompat.getDrawable(
+                            requireContext(),
+                            R.drawable.pp_taufik
+                        )
+                    )
+                    tvProfileName.text = profile.profileName
+                    tvProfileJob.text = profile.profileJob
+                    tvProfileDesc.text = profile.profileDesc
+                    tvProfileEmail.text = profile.profileEmail
+                    tvProfileOffice.text = profile.profileOffice
+                }
+            }
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
+        _binding = null
     }
 }
