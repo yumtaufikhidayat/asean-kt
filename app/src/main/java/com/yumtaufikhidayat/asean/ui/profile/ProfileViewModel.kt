be35b5b030ea85e7dd77025b9f01956e4107e9bd
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-08-06 19:58:51 +0700

    Add profile view model class.

---
+++ app/src/main/java/com/yumtaufikhidayat/asean/ui/profile/ProfileViewModel.kt
@@ -0,0 +1,24 @@
+package com.yumtaufikhidayat.asean.ui.profile
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.yumtaufikhidayat.asean.data.repository.AseanRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ProfileViewModel @Inject constructor(
+    private val repository: AseanRepository
+) : ViewModel() {
+
+    init {
+        insertProfile()
+    }
+
+    private fun insertProfile() = viewModelScope.launch {
+        repository.insertProfile()
+    }
+
+    fun getProfile() = repository.getProfile()
+}
