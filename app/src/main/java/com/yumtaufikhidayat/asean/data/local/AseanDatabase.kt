757760ef5c0d3472e4c7f1fb8736634ca1a32d56
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-08-06 19:53:55 +0700

    Add profile entity class to database class.

--- app/src/main/java/com/yumtaufikhidayat/asean/data/local/AseanDatabase.kt
+++ app/src/main/java/com/yumtaufikhidayat/asean/data/local/AseanDatabase.kt
@@ -3,9 +3,13 @@ package com.yumtaufikhidayat.asean.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.yumtaufikhidayat.asean.model.Country
+import com.yumtaufikhidayat.asean.model.Profile
 
 @Database(
-    entities = [Country::class],
+    entities = [
+        Country::class,
+        Profile::class
+    ],
     version = 1,
     exportSchema = false
 )
