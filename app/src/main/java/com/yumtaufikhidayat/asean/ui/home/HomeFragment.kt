987b03151d9755618b4296279b7a575ed0728dc5
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-08-07 10:03:01 +0700

    Update app version text.

--- app/src/main/java/com/yumtaufikhidayat/asean/ui/home/HomeFragment.kt
+++ app/src/main/java/com/yumtaufikhidayat/asean/ui/home/HomeFragment.kt
@@ -1,10 +1,13 @@
 package com.yumtaufikhidayat.asean.ui.home
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -15,7 +18,9 @@ import com.yumtaufikhidayat.asean.databinding.FragmentHomeBinding
 import com.yumtaufikhidayat.asean.model.Country
 import com.yumtaufikhidayat.asean.ui.detail.DetailFragment
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlin.time.Duration.Companion.seconds
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
@@ -23,12 +28,26 @@ class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
 
-    private val countriesAdapter by lazy { CountriesAdapter {
-        navigateToDetail(it)
-    }}
-
     private val viewModel by viewModels<HomeViewModel>()
+    private val countriesAdapter by lazy { CountriesAdapter { navigateToDetail(it) }}
 
+    private var doubleBackToExitPressedOnce = false
+    private val backPressedCallback: OnBackPressedCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            if (doubleBackToExitPressedOnce) {
+                requireActivity().finish()
+                return
+            }
+
+            doubleBackToExitPressedOnce = true
+            Toast.makeText(requireContext(), "Press back again to exit", Toast.LENGTH_SHORT).show()
+
+            lifecycleScope.launch {
+                delay(2.seconds)
+                doubleBackToExitPressedOnce = false
+            }
+        }
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -39,7 +58,10 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        requireActivity().onBackPressedDispatcher.addCallback(
+            viewLifecycleOwner,
+            backPressedCallback
+        )
         initAdapter()
         getAllCountries()
         navigateToProfile()
