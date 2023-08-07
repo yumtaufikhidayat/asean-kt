2eaa3553a99722bb261753bc69e9b747ffe894fe
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-08-07 11:02:03 +0700

    Add nullable into each variable.

--- app/src/main/java/com/yumtaufikhidayat/asean/model/Profile.kt
+++ app/src/main/java/com/yumtaufikhidayat/asean/model/Profile.kt
@@ -6,9 +6,10 @@ import androidx.room.PrimaryKey
 @Entity
 data class Profile(
     @PrimaryKey
-    val profileName: String,
-    val profileJob: String,
-    val profileDesc: String,
-    val profileEmail: String,
-    val profileOffice: String
+    val profileId: Int,
+    val profileName: String?,
+    val profileJob: String?,
+    val profileDesc: String?,
+    val profileEmail: String?,
+    val profileOffice: String?
 )
