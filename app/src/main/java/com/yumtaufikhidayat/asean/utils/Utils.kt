e4146c9d0bc8cd83f146a93ea2c4edcec955e636
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-08-07 09:43:43 +0700

    Add app version extension function and add app version string resource.

--- app/src/main/java/com/yumtaufikhidayat/asean/utils/Utils.kt
+++ app/src/main/java/com/yumtaufikhidayat/asean/utils/Utils.kt
@@ -1,6 +1,9 @@
 package com.yumtaufikhidayat.asean.utils
 
+import android.content.Context
+import android.content.pm.PackageManager
 import android.widget.ImageView
+import android.widget.TextView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.yumtaufikhidayat.asean.R
@@ -12,5 +15,15 @@ object Utils {
             .placeholder(R.color.purple_700)
             .apply(RequestOptions().override(55, 55))
             .into(this)
+    }
+
+    fun TextView.appVersion(context: Context) {
+        try {
+            val pInfo = context.packageManager?.getPackageInfo(context.packageName.toString(), 0)
+            val appVersion = pInfo?.versionName
+            this.text = appVersion
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
+        }
     }
 }
